cbc25479ea2852edae45b79c77b3949879ca09c4
Thomas <tcarbonell12@gmail.com>
2025-04-23 19:01:07 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
+require('datejs');
 
+
+function combineUsers(...args) {
+  let combinedObject = {
+    "users": [],
+    "merge_date": Date.today().toString("M/d/yyyy")
+  };
+  
+
+  //outer loop grabs all the arrays that could be passed into combineUsers
+  for (let usersArray of args) {
+    //inner loop takes each index of any array that could be passed into combineUsers, and adds them as a value for "users":
+    for (let user of usersArray) {
+      combinedObject.users.push(user);
+    }
+  }
+  
+
+  console.log(combinedObject);
+  return combinedObject;
+}
+
+combineUsers([1, 27, 14, 2384], ["jerry", "mark"]);
 
 
 module.exports = {
